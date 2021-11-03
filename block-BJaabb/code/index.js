db2dd4fb4bc966e4f42857e31308b406bd707274
dhairyasheel <dhairyasheeln@gmail.com>
2021-11-04 00:10:08 +0530

    JS Assignment 2

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,22 +6,48 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user=prompt("Enter your name.");
+alert(user);
+let userName=user;
+console.log(user,userName);
+let age=22,isLearning=true;
+user=`John`;
+console.log(user,userName);
+
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
-
+let numA=Number(prompt("Enter first number"));
+let numB=Number(prompt("Enter second number"));
+let sum;
+sum=numA+numB;
+alert(sum);
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10;j=j+2) console.log(j);
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
-
+for(let i=9;i>=1;i=i-2) console.log(i)
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
-
+let count=5;
+let result="";
+while(count>=0)
+{
+    result=result+String(count);
+    count--;
+}
+console.log(result);
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
-
+let number=10;
+let even_concat="";
+while(number>=0){
+    even_concat=even_concat+String(number);
+    number=number-2;
+}
+console.log(even_concat);
 // Comparisoin
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
@@ -36,3 +62,14 @@ Example:
 "hello", -21; // false
 
 */
+let num1=+prompt("Enter first number");
+let num2=+prompt("Enter second number");
+if(num1===num2){
+    alert("true");
+}
+else if(num1!==num2){
+    alert("false");
+}
+else{
+    alert("Enter Valid value");
+}
